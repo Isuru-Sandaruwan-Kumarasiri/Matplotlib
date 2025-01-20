082531ec9bbfcb3b3d9486c63a30b968204c5e89
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2025-01-20 22:37:31 +0530

    bar chart

---
+++ bar_chart.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+
+##   Line Chart
+
+plt.figure()
+
+age =[23,34,24,25,64,53,45]
+weight=[38,55,45,71,43,55,65]
+
+plt.subplot(1,2,1)
+plt.plot(age,weight,'r--')
+plt.title("BMI")
+plt.xlabel("Age")
+plt.ylabel("Weight")
+
+
+
+##   Bar chart
+
+plt.subplot(1,2,2)
+subjects=['maths','physics','chemistry']
+marks=[70,80,55]
+
+plt.barh(subjects,marks)
+# plt.bar(subjects,marks,width=0.4)
+plt.title("Student Marks")
+plt.xlabel("Subject")
+plt.ylabel("Marks")
+
+
+# Display the plot
+plt.tight_layout()  # Adjust layout to prevent overlap
+plt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+plt.barh(subjects,marks)
